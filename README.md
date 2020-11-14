32b9d4eca8f1cc969647e019a0ca8564d27baf23
John Lamis <lamisjohn@gmail.com>
2020-11-14 19:07:01 +0000

    add: cmd arguments update: README

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 ## Prerequisites
 This has been tested on linux on the Debian Buster distro, but should work on all linux systems.  
 Also has been tested on MacOS.  
-The dataset to run the program must be present in the project root and be named W_Dataset.csv.  
-Alternatively, you can change the name in the main.cpp file under the src directory and compile again.
+The base path of the program is assumed to be the project root. E.g. if the dataset.csv is passed  
+as a command line argument, then the file dataset.csv must be present in the project root.
 ## Build instructions
 * Run the build_project.sh script in the project root.
 * Run the executable named project in the Release directory. This MUST be run from the project root  
@@ -12,3 +12,10 @@ like ./Release/project, or the paths will be wrong.
 # Testing
 ## Instructions
 * Run the run_tests.sh script in the project root.
+
+# Command line arguments
+* --in: This controls the input file name. Default: W_Dataset.csv
+* --out: This controls the output file name. Default: W_Out_Pairs.csv
+* --type: This controls whether the results are written as pairs or as a list matching ids.  
+Default value: pairs  
+Allowed values: pairs, all
--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,13 @@
 #include "Set.hpp"
 #include "Utils.hpp"
 
-void PairsToClique(CustomVector<Pair *> * pairs, Clique* clique){
+struct ProgramParams {
+  std::string outName = "W_Out_Pairs.csv";
+  std::string outType = "pairs";
+  std::string inName = "W_Dataset.csv";
+};
+
+void PairsToClique(CustomVector<Pair*>* pairs, Clique* clique) {
   for (auto i = 0; i < pairs->getLength(); i++) {
     auto pair = (*pairs)[i];
     if (pair->value == 0) {
@@ -21,12 +27,12 @@ void PairsToClique(CustomVector<Pair *> * pairs, Clique* clique){
   }
 }
 
-List<Entry<Set*>*>* RemoveDup(List<Entry<Set*>*>* entries){
+List<Entry<Set*>*>* RemoveDup(List<Entry<Set*>*>* entries) {
   HashMap<Set*> dedupe;
   for (auto j = entries->getRoot(); j != nullptr; j = *(j->getNext())) {
     auto val = j->getValue();
     auto item = val->value;
-    std::string key = std::to_string((intptr_t) item);
+    std::string key = std::to_string((intptr_t)item);
     dedupe.set(key, item);
     delete val;
   }
@@ -34,18 +40,47 @@ List<Entry<Set*>*>* RemoveDup(List<Entry<Set*>*>* entries){
   return dedupe.getEntries();
 }
 
-int main() {
+void parseArgs(int argc, char** argv, ProgramParams* params) {
+  for (int i = 0; i < argc; i++) {
+    if (i == (argc - 1)) {
+      continue;
+    }
+    auto val = std::string(argv[i]);
+    if (val == "--out") {
+      params->outName = std::string(argv[i + 1]);
+    }
+    if (val == "--in") {
+      params->inName = std::string(argv[i + 1]);
+    }
+    if (val == "--type") {
+      auto type = std::string(argv[i + 1]);
+      if (type != "pairs" && type != "all") {
+        std::cout << "Valid values for the type argument are: pairs, all\n";
+        exit(EXIT_FAILURE);
+      }
+      params->outType = type;
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+  ProgramParams params;
+  parseArgs(argc, argv, &params);
   int len = 2048;
   char cwd[len];
   getcwd(cwd, len);
-  auto path = FileSystem::join(cwd, "W_Dataset.csv");
+  auto path = FileSystem::join(cwd, params.inName);
   auto clique = new Clique();
   auto pairs = CSV::ReadCSV(path);
   PairsToClique(pairs, clique);
   auto entries = clique->getEntries();
   auto unique = RemoveDup(entries);
-  auto file = FileSystem::join(cwd, "W_Out_Pairs.csv");
-  CSV::WriteCSV(file, unique);
+  auto file = FileSystem::join(cwd, params.outName);
+  if (params.outType == "pairs") {
+    CSV::WriteCSVPairs(file, unique);
+  } else if (params.outType == "all") {
+    CSV::WriteCSV(file, unique);
+  }
   for (auto i = 0; i < pairs->getLength(); i++) {
     auto pair = (*pairs)[i];
     delete pair;
