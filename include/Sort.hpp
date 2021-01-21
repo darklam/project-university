19643d907efa5ff6ee5e79b4e60ebaa378a1f887
doubdoub <kon_doubas98@hotmail.com>
2021-01-21 19:54:41 +0200

    added code to extract max n features from tfidf vec

---
+++ include/Sort.hpp
@@ -0,0 +1,12 @@
+#ifndef SORT_HPP
+#define SORT_HPP
+#include "FastVector.hpp"
+#include "HashMap.hpp"
+
+namespace Sort {
+    void sort(int length, FastVector<Entry<int>*>& src, FastVector<int>& dest);
+}
+
+
+#endif
+
