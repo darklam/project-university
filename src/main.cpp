5c61b2867a012e3a89cadb325fa18e7356f0edc7
John Lamis <lamisjohn@gmail.com>
2021-01-24 15:54:26 +0000

    fix: formatting

--- src/main.cpp
+++ src/main.cpp
@@ -22,36 +22,31 @@
 
 #define BATCH_SIZE 1024
 
-struct ProgramParams
-{
+struct ProgramParams {
   std::string outName = "W_Out_Pairs.csv";
   std::string outType = "pairs";
   std::string inName = "W_Dataset.csv";
   std::string inCameras = "cameras";
 };
 
-struct probability
-{
+struct probability {
   std::string str1;
   std::string str2;
   float prob;
 };
 
-void balanceDataset(FastVector<std::string> &dataset, FastVector<std::string> &train, int size)
-{
+void balanceDataset(FastVector<std::string>& dataset,
+                    FastVector<std::string>& train,
+                    int size) {
   int positives = 0;
   int negatives = 0;
-  for (int i = 0; i < size; i++){
+  for (int i = 0; i < size; i++) {
     auto row = dataset.get(i);
-    if (row.back() == '1')
-    {
+    if (row.back() == '1') {
       train.append(row);
       positives++;
-    }
-    else
-    {
-      if (positives >= negatives + 1)
-      {
+    } else {
+      if (positives >= negatives + 1) {
         train.append(row);
         negatives++;
       }
@@ -59,16 +54,14 @@ void balanceDataset(FastVector<std::string> &dataset, FastVector<std::string> &t
   }
 }
 
-void RandomizeDataset(FastVector<std::string> &dataset)
-{
+void RandomizeDataset(FastVector<std::string>& dataset) {
   int total_size = dataset.getLength();
   int pos1;
   int pos2;
-  for (int i = 0; i < total_size; i++){
+  for (int i = 0; i < total_size; i++) {
     pos1 = rand() % total_size;
     pos2 = rand() % total_size;
-    if (pos1 == pos2)
-    {
+    if (pos1 == pos2) {
       continue;
     }
     auto temp = dataset.get(pos1);
@@ -77,78 +70,76 @@ void RandomizeDataset(FastVector<std::string> &dataset)
   }
 }
 
-void train_test_split(FastVector<std::string> &dataset, FastVector<std::string> &train, FastVector<std::string> &test, FastVector<std::string> &validation)
-{
+void train_test_split(FastVector<std::string>& dataset,
+                      FastVector<std::string>& train,
+                      FastVector<std::string>& test,
+                      FastVector<std::string>& validation) {
   RandomizeDataset(dataset);
   RandomizeDataset(dataset);
   int test_size = dataset.getLength() * 0.2 + 1;
   int validation_size = dataset.getLength() * 0.2 + 1;
   int train_size = dataset.getLength() * 0.6;
-  for (int i = 0; i < test_size; i++){
+  for (int i = 0; i < test_size; i++) {
     test.append(dataset.get(i));
   }
-  for (int i = test_size; i < test_size + validation_size; i++){
+  for (int i = test_size; i < test_size + validation_size; i++) {
     validation.append(dataset.get(i));
   }
-  for (int i = test_size + validation_size; i < dataset.getLength(); i++){
+  for (int i = test_size + validation_size; i < dataset.getLength(); i++) {
     train.append(dataset.get(i));
   }
 }
 
-void parseArgs(int argc, char **argv, ProgramParams *params)
-{
-  for (int i = 0; i < argc; i++){
-    if (i == (argc - 1))
-    {
+void parseArgs(int argc, char** argv, ProgramParams* params) {
+  for (int i = 0; i < argc; i++) {
+    if (i == (argc - 1)) {
       continue;
     }
     auto val = std::string(argv[i]);
-    if (val == "--out")
-    {
+    if (val == "--out") {
       params->outName = std::string(argv[i + 1]);
     }
-    if (val == "--in")
-    {
+    if (val == "--in") {
       params->inName = std::string(argv[i + 1]);
     }
-    if (val == "--type")
-    {
+    if (val == "--type") {
       auto type = std::string(argv[i + 1]);
-      if (type != "pairs" && type != "all")
-      {
+      if (type != "pairs" && type != "all") {
         std::cout << "Valid values for the type argument are: pairs, all\n";
         exit(EXIT_FAILURE);
       }
       params->outType = type;
     }
-    if (val == "--c")
-    {
+    if (val == "--c") {
       params->inCameras = std::string(argv[i + 1]);
     }
   }
 }
 
 template <typename T>
-int getVector(std::string row, T **vectors, HashMap<int> &ids, float **vector, int max_length)
-{
+int getVector(std::string row,
+              T** vectors,
+              HashMap<int>& ids,
+              float** vector,
+              int max_length) {
   std::string tokens[3];
   Utils::splitStringLite(std::string(row), ",", tokens, 3);
   HashResult<int> res1;
   ids.get(tokens[0], &res1);
-  if (!res1.hasValue){
+  if (!res1.hasValue) {
     printf("something bad\n");
     exit(1);
   }
   HashResult<int> res2;
   ids.get(tokens[1], &res2);
-  if (!res2.hasValue){
+  if (!res2.hasValue) {
     printf("something bad\n");
     exit(1);
   }
   auto v1 = vectors[res1.value];
   auto v2 = vectors[res2.value];
 
-  for (int i = 0; i < max_length; i++){
+  for (int i = 0; i < max_length; i++) {
     float min = v1[i] + v2[i];
     min /= 2;
     (*vector)[i] = min;
@@ -157,10 +148,13 @@ int getVector(std::string row, T **vectors, HashMap<int> &ids, float **vector, i
 }
 
 template <typename T>
-void getDatasetAsVectors(FastVector<std::string> &dataset, T **vectors,
-                         HashMap<int> &ids, FastVector<float *> &final, FastVector<int> &y_true, int max_features)
-{
-  for (int i = 0; i < dataset.getLength(); i++){
+void getDatasetAsVectors(FastVector<std::string>& dataset,
+                         T** vectors,
+                         HashMap<int>& ids,
+                         FastVector<float*>& final,
+                         FastVector<int>& y_true,
+                         int max_features) {
+  for (int i = 0; i < dataset.getLength(); i++) {
     auto row = dataset[i];
     float* vec = new float[max_features];
     int _true = getVector(row, vectors, ids, &vec, max_features);
@@ -169,71 +163,68 @@ void getDatasetAsVectors(FastVector<std::string> &dataset, T **vectors,
   }
 }
 
-void asVector(std::string str1, std::string str2, float **vectors, HashMap<int> &ids, FastVector<float> &result)
-{
+void asVector(std::string str1,
+              std::string str2,
+              float** vectors,
+              HashMap<int>& ids,
+              FastVector<float>& result) {
   HashResult<int> res1;
   ids.get(str1, &res1);
-  if (!res1.hasValue){
+  if (!res1.hasValue) {
     printf("something bad\n");
     exit(1);
   }
   HashResult<int> res2;
   ids.get(str2, &res2);
-  if (!res2.hasValue){
+  if (!res2.hasValue) {
     printf("something bad\n");
     exit(1);
   }
   auto v1 = vectors[res1.value];
   auto v2 = vectors[res2.value];
-  for (int i = 0; i < 1000; i++){
+  for (int i = 0; i < 1000; i++) {
     result.append(v1[i]);
   }
-  for (int i = 0; i < 1000; i++){
+  for (int i = 0; i < 1000; i++) {
     result.append(v2[i]);
   }
 }
 
-void resolve(Clique *clique, std::string path)
-{
-  FILE *file = fopen(path.c_str(), "r");
-  if (!file){
+void resolve(Clique* clique, std::string path) {
+  FILE* file = fopen(path.c_str(), "r");
+  if (!file) {
     printf("Shit just got real, file: %s\n", path.c_str());
     exit(EXIT_FAILURE);
   }
   bool finished = false;
-  char *line = nullptr;
+  char* line = nullptr;
   size_t len = 0;
-  while (!finished){
+  while (!finished) {
     int length = getline(&line, &len, file);
-    if (length == -1)
-    {
+    if (length == -1) {
       finished = true;
       continue;
     }
     std::string tokens[3];
     Utils::splitStringLite(std::string(line), ",", tokens, 3);
-    if (tokens[0].length() == 0 || tokens[1].length() == 0)
-    {
+    if (tokens[0].length() == 0 || tokens[1].length() == 0) {
       printf("Something really bad happened\n");
     }
     std::string str1 = tokens[0];
     std::string str2 = tokens[1];
-    if (atoi(tokens[2].c_str()) == 1)
-    {
+    if (atoi(tokens[2].c_str()) == 1) {
       clique->setPair(str1, str2);
-    }
-    else
-    {
+    } else {
       clique->NegativePair(str1, str2);
     }
   }
-  if (line != nullptr){
+  if (line != nullptr) {
     free(line);
   }
   fclose(file);
 }
 
-bool checkIfExists(std::string str1, std::string str2, HashMap<int> &existing){
+bool checkIfExists(std::string str1, std::string str2, HashMap<int>& existing) {
   HashResult<int> res1;
   existing.get(str1 + str2, &res1);
   HashResult<int> res2;
@@ -243,29 +234,28 @@ bool checkIfExists(std::string str1, std::string str2, HashMap<int> &existing){
   return false;
 }
 
-void iterative_learning(Clique *clique,
-                        Logistic<float> &model,
+void iterative_learning(Clique* clique,
+                        Logistic<float>& model,
                         FastVector<std::string> camera_ids,
                         FastVector<std::string> dataset,
-                        HashMap<int> &existing,
-                        HashMap<int> &ids,
-                        float **vectors)
-{
+                        HashMap<int>& existing,
+                        HashMap<int>& ids,
+                        float** vectors) {
   float threshold = 0.01;
   float step = 0.5;
   bool flag = true;
-  while (threshold < 0.5){
-    if (flag){
+  while (threshold < 0.5) {
+    if (flag) {
       flag = false;
       int b = dataset.getLength() / BATCH_SIZE;
       int remaining = dataset.getLength() % BATCH_SIZE;
       FastVector<int> batches(b + 1);
-      for (int i = 0; i < b; i++){
+      for (int i = 0; i < b; i++) {
         batches.append(BATCH_SIZE);
       }
       batches.append(remaining);
       model.fit(dataset, vectors, ids, batches, 0.01);
-    }else{
+    } else {
       auto positives = clique->getPositiveEntries();
       auto pos_unique = Pairs::RemoveDup(positives);
       auto negatives = clique->getNegativeEntries();
@@ -281,29 +271,32 @@ void iterative_learning(Clique *clique,
       int b = dataset.getLength() / BATCH_SIZE;
       int remaining = dataset.getLength() % BATCH_SIZE;
       FastVector<int> batches(b + 1);
-      for (int i = 0; i < b; i++){
+      for (int i = 0; i < b; i++) {
         batches.append(BATCH_SIZE);
       }
       batches.append(remaining);
       model.fit(new_dataset, vectors, ids, batches, 0.01);
     }
     // FastVector<probability*> new_set(400000);
-    FILE *file;
+    FILE* file;
     file = fopen("nes_set.csv", "w+");
-    for (int i = 0; i < 1000; i++){
-      for (int j = i + 1; j < 1000; j++){
-        if (checkIfExists(camera_ids[i], camera_ids[j], existing)){
+    for (int i = 0; i < 1000; i++) {
+      for (int j = i + 1; j < 1000; j++) {
+        if (checkIfExists(camera_ids[i], camera_ids[j], existing)) {
           continue;
         }
         FastVector<float> vec(2000);
         asVector(camera_ids[i], camera_ids[j], vectors, ids, vec);
         auto prob = model.prob(vec);
-        if ((prob < threshold) || (prob > 1 - threshold)){
-          std::cout << prob << " " << " " << threshold << " " << 1 - threshold << std::endl;
-          if (prob >= 0.5){
-            fprintf(file, "%s, %s, %d\n", camera_ids[i].c_str(), camera_ids[j].c_str(), 1);
-          }else{
-            fprintf(file, "%s, %s, %d\n", camera_ids[i].c_str(), camera_ids[j].c_str(), 0);
+        if ((prob < threshold) || (prob > 1 - threshold)) {
+          std::cout << prob << " "
+                    << " " << threshold << " " << 1 - threshold << std::endl;
+          if (prob >= 0.5) {
+            fprintf(file, "%s, %s, %d\n", camera_ids[i].c_str(),
+                    camera_ids[j].c_str(), 1);
+          } else {
+            fprintf(file, "%s, %s, %d\n", camera_ids[i].c_str(),
+                    camera_ids[j].c_str(), 0);
           }
         }
       }
@@ -323,7 +316,7 @@ void iterative_learning(Clique *clique,
   delete clique;
 }
 
-int main(int argc, char **argv){
+int main(int argc, char** argv) {
   srand(time(NULL));
   ProgramParams params;
   parseArgs(argc, argv, &params);
@@ -336,7 +329,7 @@ int main(int argc, char **argv){
   getcwd(cwd_cameras, len);
   auto path_cameras = FileSystem::join(cwd_cameras, params.inCameras);
 
-  FastVector<CameraDTO *> cameras(30000);
+  FastVector<CameraDTO*> cameras(30000);
   FastVector<std::string> texts(30000);
   FastVector<std::string> camera_ids(30000);
   HashMap<int> ids(30000);
@@ -344,7 +337,7 @@ int main(int argc, char **argv){
   /* read folder with cameras and extract values */
   std::cout << "Getting cameras..." << std::endl;
   JSON::loadData(path_cameras, cameras);
-  for (int i = 0; i < cameras.getLength(); i++){
+  for (int i = 0; i < cameras.getLength(); i++) {
     auto str = cameras[i]->getAllProperties();
     texts.append(str);
     camera_ids.append(cameras[i]->getId());
@@ -358,20 +351,20 @@ int main(int argc, char **argv){
   TfIdfVectorizer v;
   std::cout << "Fitting the vectorizer...\n";
   v.fit(tokenized, 1000);
-  FastVector<Entry<WordInfo *> *> vec;
+  FastVector<Entry<WordInfo*>*> vec;
   v.getVocab(vec);
   int vocab_size = vec.getLength();
   std::cout << "Vocabulary size: " << vec.getLength() << std::endl;
-  float **vectors = new float *[texts.getLength()];
+  float** vectors = new float*[texts.getLength()];
   std::cout << "Transforming...\n";
   v.transform(tokenized, vectors);
 
   /* delete unwanted variables */
-  for (int i = 0; i < tokenized->getLength(); i++){
+  for (int i = 0; i < tokenized->getLength(); i++) {
     delete (*tokenized)[i];
   }
   delete tokenized;
-  for (int i = 0; i < vec.getLength(); i++){
+  for (int i = 0; i < vec.getLength(); i++) {
     auto entry = vec.get(i);
     delete entry;
   }
@@ -379,16 +372,18 @@ int main(int argc, char **argv){
   /* read csv */
   std::cout << "Reading w_dataset..." << std::endl;
   auto pairs = CSV::ReadCSV(path_dataset);
-  Clique *clique = new Clique();
+  Clique* clique = new Clique();
   HashMap<int> existing_pairs(30000);
   std::cout << "Got pairs creating dataset" << std::endl;
 
-  auto _pairs = Pairs::PairsToDataset(pairs, params.outType, params.outName, clique, existing_pairs);
+  auto _pairs = Pairs::PairsToDataset(pairs, params.outType, params.outName,
+                                      clique, existing_pairs);
   FastVector<std::string> dataset(100000);
   _pairs->values(dataset);
   delete _pairs;
   int dataset_size = dataset.getLength();
-  std::cout << "Done with dataset, final number of rows: " << dataset_size << std::endl;
+  std::cout << "Done with dataset, final number of rows: " << dataset_size
+            << std::endl;
   FastVector<std::string> train_set(300000);
   FastVector<std::string> val_set(50000);
   FastVector<std::string> test_set(50000);
@@ -404,7 +399,7 @@ int main(int argc, char **argv){
   int b = train_set.getLength() / BATCH_SIZE;
   int remaining = train_set.getLength() % BATCH_SIZE;
   FastVector<int> batches(b + 1);
-  for (int i = 0; i < b; i++){
+  for (int i = 0; i < b; i++) {
     batches.append(BATCH_SIZE);
   }
   batches.append(remaining);
@@ -417,33 +412,45 @@ int main(int argc, char **argv){
   std::cout << "\nTesting train_set...\n";
   FastVector<int> train_labels(10000);
   auto train_pred = model.predict(train_set, vectors, ids, train_labels);
-  std::cout << "\nF1: " << Metrics::f1_score(train_labels, train_pred) << std::endl;
-  std::cout << "Precision: " << Metrics::precision_score(train_labels, train_pred) << std::endl;
-  std::cout << "Recall: " << Metrics::recall_score(train_labels, train_pred) << std::endl;
-  std::cout << "Accuracy: " << Metrics::accuracy_score(train_labels, train_pred) << std::endl;
+  std::cout << "\nF1: " << Metrics::f1_score(train_labels, train_pred)
+            << std::endl;
+  std::cout << "Precision: "
+            << Metrics::precision_score(train_labels, train_pred) << std::endl;
+  std::cout << "Recall: " << Metrics::recall_score(train_labels, train_pred)
+            << std::endl;
+  std::cout << "Accuracy: " << Metrics::accuracy_score(train_labels, train_pred)
+            << std::endl;
   delete[] train_pred;
 
-  iterative_learning(clique, model, camera_ids, train_set, existing_pairs, ids, vectors);
+  iterative_learning(clique, model, camera_ids, train_set, existing_pairs, ids,
+                     vectors);
 
   std::cout << "\nTesting validation set...\n";
   FastVector<int> val_labels(10000);
   auto val_pred = model.predict(test_set, vectors, ids, val_labels);
   std::cout << "\nF1: " << Metrics::f1_score(val_labels, val_pred) << std::endl;
-  std::cout << "Precision: " << Metrics::precision_score(val_labels, val_pred) << std::endl;
-  std::cout << "Recall: " << Metrics::recall_score(val_labels, val_pred) << std::endl;
-  std::cout << "Accuracy: " << Metrics::accuracy_score(val_labels, val_pred) << std::endl;
+  std::cout << "Precision: " << Metrics::precision_score(val_labels, val_pred)
+            << std::endl;
+  std::cout << "Recall: " << Metrics::recall_score(val_labels, val_pred)
+            << std::endl;
+  std::cout << "Accuracy: " << Metrics::accuracy_score(val_labels, val_pred)
+            << std::endl;
   delete[] val_pred;
 
   std::cout << "\nTesting test set...\n";
   FastVector<int> test_labels(10000);
   auto test_pred = model.predict(test_set, vectors, ids, test_labels);
-  std::cout << "\nF1: " << Metrics::f1_score(test_labels, test_pred) << std::endl;
-  std::cout << "Precision: " << Metrics::precision_score(test_labels, test_pred) << std::endl;
-  std::cout << "Recall: " << Metrics::recall_score(test_labels, test_pred) << std::endl;
-  std::cout << "Accuracy: " << Metrics::accuracy_score(test_labels, test_pred) << std::endl;
+  std::cout << "\nF1: " << Metrics::f1_score(test_labels, test_pred)
+            << std::endl;
+  std::cout << "Precision: " << Metrics::precision_score(test_labels, test_pred)
+            << std::endl;
+  std::cout << "Recall: " << Metrics::recall_score(test_labels, test_pred)
+            << std::endl;
+  std::cout << "Accuracy: " << Metrics::accuracy_score(test_labels, test_pred)
+            << std::endl;
   delete[] test_pred;
   printf("here\n");
-  for (int i = 0; i < texts.getLength(); i++){
+  for (int i = 0; i < texts.getLength(); i++) {
     delete[] vectors[i];
   }
   delete[] vectors;
